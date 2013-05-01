2fd6feabdcfa6fe461e2100c8e7a314488571909
DBC student <apprentice@dbc05.local>
2013-05-01 12:29:30 -0700

    added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ActiveRecord::Base
-  # Remember to create a migration!
+  
+  belongs_to :user
+  has_many :comments
+
+  validates :title, :content, :presence => true
+  before_create :clean_title
+
+
+
+  def clean_title
+    self.title = title.split(" ").map { |x| x.capitalize }.join(" ")
+  end
+
+
 end
